d129c76770b28e7d811c12c4b3f67d905d318587
SunnyQjm <qjm3662@mail.dlut.edu.cn>
2019-07-07 22:05:57 +0800

    图片裁剪功能添加动态权限请求

--- quick_android_image_picker/src/main/java/cn/qjm253/quick_android_image_picker/fragment/QuickAndroidClipImageFragment.kt
+++ quick_android_image_picker/src/main/java/cn/qjm253/quick_android_image_picker/fragment/QuickAndroidClipImageFragment.kt
@@ -1,5 +1,6 @@
 package cn.qjm253.quick_android_image_picker.fragment
 
+import android.Manifest
 import android.annotation.SuppressLint
 import android.content.Intent
 import android.graphics.Bitmap
@@ -34,6 +35,8 @@ class QuickAndroidClipImageFragment : BaseQuickAndroidFragment() {
         // 裁剪后的图片路径
         const val QUICK_ANDROID_CLIP_IMAGE_RESULT = "QUICK_ANDROID_CLIP_IMAGE_RESULT"
 
+        const val QUICK_ANDROID_CLIP_IMAGE_RW_PERMISSION_REQUEST_CODE = 1
+
         const val DEFAULT_MULTIPLE = 0.3f
         fun newInstance(path: String, multiple: Float = 0.3f): QuickAndroidClipImageFragment {
             return newInstance_(
@@ -54,16 +57,16 @@ class QuickAndroidClipImageFragment : BaseQuickAndroidFragment() {
         multiple = arguments?.getFloat(QUICK_ANDROID_CLIP_IMAGE_MULTIPLE, DEFAULT_MULTIPLE) ?: DEFAULT_MULTIPLE
 
         // 检查边界，限制在 0~1之间
-        if(multiple < 0) {
+        if (multiple < 0) {
             multiple = 0.1f
-        } else if(multiple > 1f) {
+        } else if (multiple > 1f) {
             multiple = 1f
         }
         clipImageLayout.setImagePath(imagePath)
     }
 
     @SuppressLint("CheckResult")
-    fun doClip() {
+    private fun doCLip_() {
         Observable.create { emitter: ObservableEmitter<File> ->
             try {
                 FileUtils.generatePictureFile("clip.png").apply {
@@ -81,7 +84,7 @@ class QuickAndroidClipImageFragment : BaseQuickAndroidFragment() {
             }
         }
             .compose(RxSchedulersHelper.io_main())
-            .subscribe({file ->
+            .subscribe({ file ->
                 Intent().let {
                     it.putExtra(QUICK_ANDROID_CLIP_IMAGE_RESULT, file.absolutePath)
                     activity?.setResult(0, it)
@@ -91,6 +94,22 @@ class QuickAndroidClipImageFragment : BaseQuickAndroidFragment() {
             }, {
                 it.e()
             })
+    }
+
+    fun doClip() {
+        requestPermissions(
+            arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE),
+            R.string.clip_image_permission_tip, QUICK_ANDROID_CLIP_IMAGE_RW_PERMISSION_REQUEST_CODE
+        )
+    }
+
+    override fun onPermissionsGranted(requestCode: Int, perms: MutableList<String>) {
+        super.onPermissionsGranted(requestCode, perms)
+        when (requestCode) {
+            QUICK_ANDROID_CLIP_IMAGE_RW_PERMISSION_REQUEST_CODE -> {
+                doCLip_()
+            }
+        }
     }
 
     override fun initialLoadData() {
